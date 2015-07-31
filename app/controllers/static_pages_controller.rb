25cf57f1f8b220de4fb24424f92e9b398cf6d527
Vojtus <jirivojta@email.cz>
2015-07-31 17:27:14 +0200

    add footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,36 @@
 class StaticPagesController < ApplicationController
+
+  def home
+
+  end
+
+  def program
+
+  end
+
+  def novinky
+
+  end
+
+  def o_daiquiri
+
+  end
+
+  def galerie
+
+  end
+
+  def napojivy_listek
+
+  end
+
+  def program
+  end
+
+  def provozni_rad
+  end
+  
+  def kontakt
+
+  end
 end
