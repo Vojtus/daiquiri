80370f8fa6694cf33cc74842ea769bfc470baafd
Vojtus <jirivojta@email.cz>
2015-07-31 21:17:22 +0200

    boj s JS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
-  get   'program' => 'static_pages#program'
-  get   'novinky' => 'static_pages#novinky'
+  get   'program'     => 'static_pages#program'
+  get   'novinky'     => 'static_pages#novinky'
+  get   'o_daiquiri'  => 'static_pages#o_daiquiri'
+  get   'kontakt'     => 'static_pages#kontakt'
 end
