feaea141a4b1e4f4513705fe25fa83403f301fea
Vojtus <jirivojta@email.cz>
2015-08-03 17:46:55 +0200

    did nothing, trying to fix javascript collisions

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Daiquiri
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Disable precompilation
+    config.assets.precompile = []
   end
 end
